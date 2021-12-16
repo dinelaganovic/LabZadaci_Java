afa25591a389c5db28d828d13d10a813ad3eaca8
Dinela <59210189+dinelaganovic@users.noreply.github.com>
2021-12-16 02:19:39 +0100

    Update Main.java

--- Zadatak02-5/src/Main.java
+++ Zadatak02-5/src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		Mapa M1=new Mapa(4);
-        M1.setNiz(0,new Tacka(261,350,"Nis"));
-        M1.setNiz(1,new Tacka(144,175,"Beograd"));
-        M1.setNiz(2,new Tacka(90,120,"Novi Sad"));
-        M1.setNiz(3,new Tacka(77,20,"Subotica"));
-        System.out.println(M1.UkupnoRastojanje());
-        M1.IzracunajVreme(100);
-
+	        Mapa M1=new Mapa(4);
+		int ukupnorastojanje = 0;
+		try {
+			M1.setNiz(0,new Tacka(261,350,"Nis"));
+	        M1.setNiz(1,new Tacka(144,175,"Beograd"));
+	        M1.setNiz(2,new Tacka(90,120,"Novi Sad"));
+	        M1.setNiz(3,new Tacka(77,20,"Subotica"));
+	        ukupnorastojanje = M1.UkupnoRastojanje();
+	        M1.IzracunajVreme(100);
+		}catch(Exception e) {
+			System.out.println("Izuzetak:"+e);
+		}
 	}
 
 }
