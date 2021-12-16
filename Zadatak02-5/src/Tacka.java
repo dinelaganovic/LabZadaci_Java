b15c81900eefe6200b2dc4f54742d5b80f238b0a
Dinela <59210189+dinelaganovic@users.noreply.github.com>
2021-12-16 02:00:09 +0100

    Update Tacka.java

--- Zadatak02-5/src/Tacka.java
+++ Zadatak02-5/src/Tacka.java
@@ -1,16 +1,11 @@
 import static java.lang.Math.sqrt;
 public class Tacka {
-	private int  x;
-	private int y;
-	private String name;
-	public Tacka(int x,int y, String name) throws Exception{
-		try {
+	if(x < 0 || y<0)
+			throw (new Exception("Kordinata mora biti pozitivan broj!"));
+		else
 			this.x=x;
 		    this.y=y;
 		    this.name=name;
-		}catch(Exception e) {
-			System.out.println ("Kordinata mora biti pozitivan broj!");
-		}
 	    }
 	public double Rastojanje(Tacka P1){
 	    return sqrt((this.x-P1.x)*(this.x-P1.x)+(this.y-P1.y)*(this.y-P1.y));
